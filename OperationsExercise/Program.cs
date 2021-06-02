cf963daeef408d628896c040da81bd954447d80a
mattiyee <mattiyee610@gmail.com>
2021-06-02 10:00:12 -0400

    initial commit

---
+++ OperationsExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OperationsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                if (remainder == 1)
+                {
+                    Console.WriteLine("The equation is valid.");
+                }
+                else
+                {
+                    Console.WriteLine("The equation is not valid.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The values are not sufficient.");
+            }
+        }
+    }
+}
